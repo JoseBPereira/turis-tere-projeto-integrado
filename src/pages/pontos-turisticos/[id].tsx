394bc13130c08a9de0563d0619b4533c6ab75cb2
jose.dsn.pack <jose.dsn.pack@alterdata.com.br>
2022-10-15 19:33:31 -0300

    Ajustes visuais e criado as entidades no banco de dados

--- src/pages/pontos-turisticos/[id].tsx
+++ src/pages/pontos-turisticos/[id].tsx
@@ -17,8 +17,8 @@ const Detalhe: NextPageWithLayout = () => {
     const [isLoading, setLoading] = useState(true);
 
     const carregarPontoTuristico = async (id: string) : Promise<void> => {
-        // let pontoTuristico = await getPontoTuristico(id);
-        // setPontoTuristico(pontoTuristico);
+        let pontoTuristico = await getPontoTuristico(id);
+        setPontoTuristico(pontoTuristico);
         setLoading(false);
     };
 
@@ -34,6 +34,8 @@ const Detalhe: NextPageWithLayout = () => {
     };
 
     const carregarConteudo = () : ReactNode => {
+      if(!pontoTuristico) return;
+
       return (
         <div className={styles.container}>
           <button className={styles.btnVoltar} onClick={() => router.back()}>
@@ -46,9 +48,9 @@ const Detalhe: NextPageWithLayout = () => {
           </button>
           <div className={styles.containerBody}>
             <div className={styles.containerCabecalho}>
-              <span className={styles.txtNome}>Parc Magique</span>
+              <span className={styles.txtNome}>{pontoTuristico.nome}</span>
               <FaMapMarkerAlt
-                size={25}
+                size={35}
                 style={{
                   height: "100%",
                   marginRight: 15,
@@ -59,27 +61,14 @@ const Detalhe: NextPageWithLayout = () => {
               />
 
               <span>
-                R. Marcos Antônio da Silva Lima, 300 - Vargem Grande,
-                Teresópolis - RJ, 25990-150
+              {pontoTuristico.localizacao}
               </span>
             </div>
             <div className={styles.containerDescricao}>
-              <span>
-                {`O Le Canton é o único resort do Brasil que conta com um parque de diversões coberto. O Parc Magique é uma das principais atrações para crianças na Região Serrana do Rio.
-
-              Com decoração medieval, o parque oferece variedade de atrações, com cerca de 15 brinquedos, além de locais para alimentação e loja com lembranças do lugar.
-
-              Entre os destaques, estão a roda gigante, barco viking infantil, duas salas de realidade virtual de última geração com mais de 90 jogos diferentes, um simulador de montanha russa, e a Just Jump, cama elástica gigante, com 120 metros quadrados.`}
-              </span>
+              <span>{pontoTuristico.descricao}</span>
             </div>
             <div className={styles.containerImages}>
-              <Images
-                arrayImages={[
-                  "https://firebasestorage.googleapis.com/v0/b/turistere.appspot.com/o/estatua-teresa-cristina-teresopolis-rj-39.jpg.webp?alt=media&token=046c1302-d7b5-4457-891a-98e2248dd0e6",
-                  "https://firebasestorage.googleapis.com/v0/b/turistere.appspot.com/o/teresopolis-rj.jpg?alt=media&token=630a03d1-1f6e-40ed-9a04-cdebb8991e1f",
-                  "https://firebasestorage.googleapis.com/v0/b/turistere.appspot.com/o/teresopolis-rj.jpg?alt=media&token=630a03d1-1f6e-40ed-9a04-cdebb8991e1f",
-                ]}
-              ></Images>
+              <Images arrayImages={pontoTuristico.fotos}></Images>
             </div>
           </div>
         </div>
